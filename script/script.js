367821867c022d9c9ae0f853608590b8bfe0c0e2
Nacho-sf <serranof.ignacio@gmail.com>
2022-07-20 17:53:46 +0200

    add/remodel_logica

--- script/script.js
+++ script/script.js
@@ -1,130 +1,81 @@
-// const quizData = [
-//     {
-//         question: "¿Quién muere en el primer episodio de la primera temporada?",
-//         a: "Joffry Baratheon",
-//         b: "Eddard Stark",
-//         c: "Tyrion Lannister",
-//         d: "Nadie muere",
-//         correct: "b",
-//     },
-//     {
-//         question: "¿Quién es la madre de Jon Stark?",
-//         a: "Sansa Stark",
-//         b: "Catelyn Stark",
-//         c: "Cersei Lannister",
-//         d: "Ni Jon lo sabe",
-//         correct: "d",
-//     },
-//     {
-//         question: "¿Cuál de las siguientes NO es una familia de Juego de tronos?",
-//         a: "Tyrell",
-//         b: "Bolton",
-//         c: "Borbon",
-//         d: "Stark",
-//         correct: "c",
-//     },
-//     {
-//         question: "¿Cuándo recibió Daenerys sus huevos de dragón?",
-//         a: "En Astapor, como regalo de bienvenida",
-//         b: "Con Khal Drogo al saber que iba a ser padre",
-//         c: "Fue un regalo de boda de Magister Ilyrio",
-//         d: "Se los encontraron por el camino",
-//         correct: "d",
-//     },
-  
-  
-//   ];
 
 
-
-//   const quiz= document.getElementById('quiz')
-//   const answerEls = document.querySelectorAll('.answer')
-//   const questionEl = document.getElementById('question')
-//   const a_text = document.getElementById('a_text')
-//   const b_text = document.getElementById('b_text')
-//   const c_text = document.getElementById('c_text')
-//   const d_text = document.getElementById('d_text')
-//   const submitBtn = document.getElementById('submit')
-  
-  
-//   let currentQuiz = 0
-//   let score = 0
-  
-//   loadQuiz()
-  
-//   function loadQuiz() {
-  
-//       //deselectAnswers()
-  
-//     const currentQuizData = quizData[currentQuiz]
-  
-//     questionEl.innerText = currentQuizData.question
-//     a_text.innerText = currentQuizData.a
-//     b_text.innerText = currentQuizData.b
-//     c_text.innerText = currentQuizData.c
-//     d_text.innerText = currentQuizData.d
-//   }
-//   /*
-//   function deselectAnswers() {
-//       answerEls.forEach(answerEl => answerEl.focus = false)
-//   }
-//   */
-  
-//   function getSelected() {
-//     let answer
-//     answerEls.forEach(answerEl => {
-//         if(answerEl.checked) {
-//             answer = answerEl.id
-//         }
-//     })
-//     return answer
-//   }
-  
-  
-  
-//   submitBtn.addEventListener('click', () => {
-//       const answer = getSelected()
-//       if(answer) {
-//          if(answer === quizData[currentQuiz].correct) {
-//              score++
-//          }
-  
-//          currentQuiz++
-  
-//          if(currentQuiz < quizData.length) {
-//              loadQuiz()
-//          } else {
-//              quiz.innerHTML = `
-//              <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-  
-//              <button onclick="location.reload()">Reload</button>
-//              `
-//          }
-//       }
-//   })
+let score = 0;
+let currentQuestion = 0;
+const submitBtn = document.getElementById('submit')
 
 
-
-let currentQuestion = 0;       //++ 1 addevent listener para "siguiente" y va pasando las preguntas
+       //++ 1 addevent listener para "siguiente" y va pasando las preguntas
 //función asíncrona para traer pregs y resp de la API - PTE TERMINAR!!!! dentro de esta función tiene que ir toda la lógica del quiz
 async function quizGame() {
+    
+
     try {
 
-        let responseQuiz = await fetch ('https://opentdb.com/api.php?amount=13&category=17&difficulty=medium&type=multiple')
+        let responseQuiz = await fetch('https://opentdb.com/api.php?amount=13&category=17&difficulty=medium&type=multiple')
         let pregsQuiz = await responseQuiz.json()
         console.log(pregsQuiz.results)
+
+        let quest = pregsQuiz.results[currentQuestion].question;
+        console.log(quest);
+
+
+        function paintQUest(num) {
+            let arrayresp = [...pregsQuiz.results[num].incorrect_answers, pregsQuiz.results[num].correct_answer]; //para meter las 4 respuestas
+            console.log(arrayresp);
+
+            document.getElementById('question').innerHTML = `${quest}`
+            document.getElementById('a_text').innerHTML = `${arrayresp[0]}`
+            document.getElementById('b_text').innerHTML = `${arrayresp[1]}`
+            document.getElementById('c_text').innerHTML = `${arrayresp[2]}`
+            document.getElementById('d_text').innerHTML = `${arrayresp[3]}`
         
-        let quest= pregsQuiz.results[currentQuestion].question;
-        let arrayresp = [...pregsQuiz.results[currentQuestion].incorrect_answers,pregsQuiz.results[currentQuestion].correct_answer]; //para meter las 4 respuestas
-        console.log(arrayresp);
-
-        document.getElementById('a_text').innerHTML = `${arrayresp[0]}` 
-        document.getElementById('b_text').innerHTML = `${arrayresp[1]}`  
-        document.getElementById('c_text').innerHTML = `${arrayresp[2]}` 
-        document.getElementById('d_text').innerHTML = `${arrayresp[3]}` 
-
-        let list = [0,1, 2, 3]
+        }
+        paintQUest(currentQuestion)
+        
+        let list = [0, 1, 2, 3]
         list = list.sort(() => Math.random() - 0.5) //para que la posición de la respuesta correcta vaya cambiando
+
+
+        function getSelected() {
+            let answer
+            arrayresp.forEach(answerSel => {
+                if (answerSel.checked) {
+                    answer = answerSel.id
+                }
+            })
+            return answer
+        }
+
+
+        submitBtn.addEventListener('click', () => {
+            // const answer = getSelected()
+            // if (answer) {
+                // if (answer === pregsQuiz.results[currentQuestion].correct_answer) {
+                //     score++
+                // }
+
+                currentQuestion++
+
+                paintQUest(currentQuestion)
+
+                if (currentQuestion < pregsQuiz.results.length) {
+                    quizGame()
+                } else {
+                    quiz.innerHTML = `
+              <h2>You answered ${score}/${pregsQuiz.results.length} questions correctly</h2>
+  
+              <button onclick="location.reload()">Reload</button>
+              `
+                }
+            // }
+
+        })
+
+
+
+
+
 
 
 
@@ -134,6 +85,18 @@ async function quizGame() {
 }
 
 quizGame();
+
+
+
+
+
+////// e.target.value /////
+
+
+
+
+
+
 
 /*
 
